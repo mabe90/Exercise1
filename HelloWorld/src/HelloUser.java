ddd07da812c2cb6b21adf9455c3315aeae4e3416
Marvin Becker <marvinbecker@live.de>
2016-01-05 18:27:35 +0100

    zweites mal aenderung an hellouser und helloworld

--- HelloWorld/src/HelloUser.java
+++ HelloWorld/src/HelloUser.java
@@ -5,18 +5,18 @@ public class HelloUser
 {
 	public static String userName;
 	
-	public static void main(String[] args) 
+	public static void main() 
+	{
+	    greetUser();
+	}
+	
+	public static void greetUser()
 	{
 		System.out.println("Hello ASE2015 - how are you? :)");
 		System.out.print("Please enter your name: ");
 		Scanner scanIn = new Scanner(System.in);
 	    userName = scanIn.nextLine();
 	    scanIn.close();
-	    greetUser();
-	}
-	
-	public static void greetUser()
-	{
 		System.out.println("Hello " + userName + "!");	
 	}
 
--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -8,12 +8,12 @@
  */
 public class HelloWorld {
 
-	/**
+	/** Solution
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		System.out.println("Hello ASE2015 - how are you now? :)");
-	}
 
+	public static void main(String[] args) {
+		System.out.println("Hello ASE2015 - how are you? :)");
+	}
 }
 
